dfc46f8bed8ea90e7580c119d4032c02643bcffe
Michelle Lu <mlu7@terpmail.umd.edu>
2023-05-13 17:46:51 -0400

    Added ejs folder

--- stuff.js
+++ stuff.js
@@ -1,7 +1,9 @@
+const path = require("path");
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const portNumber = process.env.PORT || 3000
 
 const userName = process.env.MONGO_DB_USERNAME;
 const password = process.env.MONGO_DB_PASSWORD;
@@ -10,15 +12,22 @@ const databaseAndCollection = {db: process.env.MONGO_DB_NAME, collection: proces
 const uri = `mongodb+srv://${userName}:${password}@cluster0.29ulgrq.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function postJSON(data) {
+app.set("templates", path.resolve(__dirname, "templates"));
+app.set("view engine", "ejs");
+
+async function postJSON() {
     try {
       const response = await fetch("https://rhodesapi.up.railway.app/api/operator/Texas");
       const result = await response.json();
       console.log("Success:", result);
+      process.exit(0)
     } catch (error) {
       console.error("Error:", error);
     }
   }
   
-  const data = { username: "example" };
-  postJSON(data);
+
+app.listen(portNumber);
+
+// for testing (below)
+postJSON();

ef7c9c2b37d5e1d3f806681a2a3d28a5d9328308
Michelle Lu <mlu7@terpmail.umd.edu>
2023-05-13 17:31:15 -0400

    Added mongo db stuff

--- stuff.js
+++ stuff.js
@@ -1,8 +1,18 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const userName = process.env.MONGO_DB_USERNAME;
+const password = process.env.MONGO_DB_PASSWORD;
+
+const databaseAndCollection = {db: process.env.MONGO_DB_NAME, collection: process.env.MONGO_COLLECTION};
+const uri = `mongodb+srv://${userName}:${password}@cluster0.29ulgrq.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function postJSON(data) {
     try {
       const response = await fetch("https://rhodesapi.up.railway.app/api/operator/Texas");
-  
       const result = await response.json();
       console.log("Success:", result);
     } catch (error) {

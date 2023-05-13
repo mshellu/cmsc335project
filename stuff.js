0e20233ec0b1b3d113fcfba5ca7059a3dfcdb7bf
Kammie Ping <kping@terpmail.umd.edu>
2023-05-13 17:55:33 -0400

    smeioclin

--- stuff.js
+++ stuff.js
@@ -3,8 +3,8 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const portNumber = process.env.PORT || 3000
-require("dotenv").config({ path: path.resolve(__dirname, '.env') }) 
+const portNumber = process.env.PORT || 3000;
+require("dotenv").config({ path: path.resolve(__dirname, '.env') })
 
 const userName = process.env.MONGO_DB_USERNAME;
 const password = process.env.MONGO_DB_PASSWORD;
